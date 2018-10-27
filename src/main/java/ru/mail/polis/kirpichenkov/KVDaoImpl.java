49f82f999dce2a864afebbc23d873c8368175347
kirpichenkov <kirpichenkovspbpu@gmail.com>
2018-10-27 14:16:01 +0300

    Google style formatting

--- src/main/java/ru/mail/polis/kirpichenkov/KVDaoImpl.java
+++ src/main/java/ru/mail/polis/kirpichenkov/KVDaoImpl.java
@@ -12,77 +12,72 @@ import java.io.IOException;
 import java.util.NoSuchElementException;
 import org.apache.commons.io.FileUtils;
 
-/**
- *  @author Pavel Kirpichenkov
- */
+/** @author Pavel Kirpichenkov */
 public class KVDaoImpl implements KVDao {
-    private final int HEX_CHAR_PER_DIR = 4;
-    private final static Logger logger = Logger.getLogger(KVDaoImpl.class);
+  private final int HEX_CHAR_PER_DIR = 4;
+  private static final Logger logger = Logger.getLogger(KVDaoImpl.class);
 
-    private final File basePath;
+  private final File basePath;
 
-    public KVDaoImpl(@NotNull File path) {
-        this.basePath = path;
+  public KVDaoImpl(@NotNull File path) {
+    this.basePath = path;
+  }
+
+  @NotNull
+  @Override
+  public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
+    File fileToRead = keyToFile(key);
+    logger.debug(String.format("get %s", fileToRead.toString()));
+    if (!fileToRead.exists() || !fileToRead.isFile()) {
+      throw new NoSuchElementException();
+    } else {
+      return IOUtils.toByteArray(new FileInputStream(fileToRead));
     }
+  }
 
-    @NotNull
-    @Override
-    public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
-        File fileToRead = keyToFile(key);
-        logger.debug(String.format("get %s", fileToRead.toString()));
-        if (!fileToRead.exists() || !fileToRead.isFile()) {
-            throw new NoSuchElementException();
-        } else {
-            return IOUtils.toByteArray(new FileInputStream(fileToRead));
-        }
+  @Override
+  public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
+    File fileToWrite = keyToFile(key);
+    logger.debug(String.format("upsert %s", fileToWrite));
+    File parentDir = fileToWrite.getParentFile();
+    if (!parentDir.exists()) {
+      if (!parentDir.mkdirs()) {
+        throw new IOException("Can't create path to file");
+      }
     }
+    fileToWrite.createNewFile();
+    FileUtils.writeByteArrayToFile(fileToWrite, value);
+  }
 
-    @Override
-    public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
-        File fileToWrite = keyToFile(key);
-        logger.debug(String.format("upsert %s", fileToWrite));
-        File parentDir = fileToWrite.getParentFile();
-        if (!parentDir.exists()) {
-            if (!parentDir.mkdirs()) {
-                throw new IOException("Can't create path to file");
-            }
-        }
-        fileToWrite.createNewFile();
-        FileUtils.writeByteArrayToFile(fileToWrite, value);
+  @Override
+  public void remove(@NotNull byte[] key) throws IOException {
+    File fileToRemove = keyToFile(key);
+    logger.debug(String.format("upsert %s", fileToRemove));
+    if (fileToRemove.exists()) {
+      if (!fileToRemove.delete()) {
+        throw new IOException("Can't remove file");
+      }
     }
+  }
 
-    @Override
-    public void remove(@NotNull byte[] key) throws IOException {
-        File fileToRemove = keyToFile(key);
-        logger.debug(String.format("upsert %s", fileToRemove));
-        if (fileToRemove.exists()) {
-            if (!fileToRemove.delete()) {
-                throw new IOException("Can't remove file");
-            }
-        }
+  @Override
+  public void close() throws IOException {}
+
+  /**
+   * Convert key from byte array to file path. The total number of subdirectories and files in one
+   * directory is limited. Long keys are transformed into directory hierarchy
+   */
+  @NotNull
+  private File keyToFile(@NotNull byte[] key) {
+    final String hexKey = DatatypeConverter.printHexBinary(key);
+    final int hexLength = hexKey.length();
+    final StringBuilder path = new StringBuilder(basePath.toString());
+
+    int ix;
+    for (ix = 0; hexLength - ix > HEX_CHAR_PER_DIR; ix += HEX_CHAR_PER_DIR) {
+      path.append('/').append(hexKey, ix, ix + HEX_CHAR_PER_DIR);
     }
-
-    @Override
-    public void close() throws IOException {
-
-    }
-
-    /**
-     * Convert key from byte array to file path
-     * The total number of subdirectories and files in one directory is limited
-     * Long keys are transformed into directory hierarchy
-     */
-    @NotNull
-    private File keyToFile(@NotNull byte[] key) {
-        final String hexKey = DatatypeConverter.printHexBinary(key);
-        final int hexLength = hexKey.length();
-        final StringBuilder path = new StringBuilder(basePath.toString());
-
-        int ix;
-        for (ix = 0; hexLength - ix > HEX_CHAR_PER_DIR ; ix += HEX_CHAR_PER_DIR) {
-            path.append('/').append(hexKey, ix, ix + HEX_CHAR_PER_DIR);
-        }
-        path.append('/').append(hexKey, ix, hexLength);
-        return new File(path.toString());
-    }
+    path.append('/').append(hexKey, ix, hexLength);
+    return new File(path.toString());
+  }
 }
--- src/main/java/ru/mail/polis/kirpichenkov/KVServiceImpl.java
+++ src/main/java/ru/mail/polis/kirpichenkov/KVServiceImpl.java
@@ -10,172 +10,180 @@ import ru.mail.polis.KVService;
 import java.io.IOException;
 import java.util.NoSuchElementException;
 
-/**
- * @author Pavel Kirpichenkov
- */
+/** @author Pavel Kirpichenkov */
 public class KVServiceImpl extends HttpServer implements KVService {
-    private KVDao dao;
-    private ServerConfig config;
-    private final static Logger logger = Logger.getLogger(KVServiceImpl.class);
+  private static final Logger logger = Logger.getLogger(KVServiceImpl.class);
+  private KVDao dao;
+  private ServerConfig config;
 
-    private KVServiceImpl(ServerConfig config) throws IOException {
-        super(config);
-    }
+  private KVServiceImpl(ServerConfig config) throws IOException {
+    super(config);
+  }
 
-    /**
-     * Get new service instance
-     *
-     * @param dao  {@link KVDao} instance
-     * @param port service port number
-     * @return new instance
-     * @throws IOException
-     */
-    public static KVServiceImpl create(KVDao dao, int port) throws IOException {
-        ServerConfig config = createConfig(port);
-        KVServiceImpl kvService = new KVServiceImpl(config);
-        kvService.dao = dao;
-        kvService.config = config;
-        return kvService;
-    }
+  /**
+   * Get new service instance
+   *
+   * @param dao {@link KVDao} instance
+   * @param port service port number
+   * @return new instance
+   * @throws IOException
+   */
+  public static KVServiceImpl create(KVDao dao, int port) throws IOException {
+    ServerConfig config = createConfig(port);
+    KVServiceImpl kvService = new KVServiceImpl(config);
+    kvService.dao = dao;
+    kvService.config = config;
+    return kvService;
+  }
 
-    public void start() {
-        super.start();
-        for (AcceptorConfig ac: config.acceptors) {
-            logger.debug(String.format("server started at %s:%d", ac.address, ac.port));
-        }
+  public void start() {
+    super.start();
+    for (AcceptorConfig ac : config.acceptors) {
+      logger.debug(String.format("server started at %s:%d", ac.address, ac.port));
     }
+  }
 
-    public void stop() {
-        super.stop();
-        logger.debug("server stopped");
-    }
+  public void stop() {
+    super.stop();
+    logger.debug("server stopped");
+  }
 
-    private static ServerConfig createConfig(int port) {
-        ServerConfig config = new ServerConfig();
-        AcceptorConfig acceptorConfig = new AcceptorConfig();
-        acceptorConfig.port = port;
-        config.acceptors = new AcceptorConfig[]{acceptorConfig};
-        return config;
-    }
+  private static ServerConfig createConfig(int port) {
+    ServerConfig config = new ServerConfig();
+    AcceptorConfig acceptorConfig = new AcceptorConfig();
+    acceptorConfig.port = port;
+    config.acceptors = new AcceptorConfig[] {acceptorConfig};
+    return config;
+  }
 
-    public void handleDefault(Request request, HttpSession session) throws IOException {
-        logger.debug(String.format("%s %s", methodToString(request), request.getURI()));
-        switch (request.getPath()) {
-            case "/v0/status":
-                handleStatus(session);
-                break;
-            case "/v0/entity":
-                handleEntity(request, session);
-                break;
-            default:
-                sendNotFound(session);
-        }
+  public void handleDefault(Request request, HttpSession session) throws IOException {
+    logger.debug(String.format("%s %s", methodToString(request), request.getURI()));
+    switch (request.getPath()) {
+      case "/v0/status":
+        handleStatus(session);
+        break;
+      case "/v0/entity":
+        handleEntity(request, session);
+        break;
+      default:
+        sendNotFound(session);
     }
+  }
 
-    private void handleEntity(Request request, HttpSession session) throws IOException {
-        switch (request.getMethod()) {
-            case Request.METHOD_GET:
-                handleGet(request, session);
-                break;
-            case Request.METHOD_PUT:
-                handlePut(request, session);
-                break;
-            case Request.METHOD_DELETE:
-                handleDelete(request, session);
-                break;
-            default:
-                sendMethodNotAllowed(session);
-        }
+  private void handleEntity(Request request, HttpSession session) throws IOException {
+    switch (request.getMethod()) {
+      case Request.METHOD_GET:
+        handleGet(request, session);
+        break;
+      case Request.METHOD_PUT:
+        handlePut(request, session);
+        break;
+      case Request.METHOD_DELETE:
+        handleDelete(request, session);
+        break;
+      default:
+        sendMethodNotAllowed(session);
     }
+  }
 
-    private void handleGet(Request request, HttpSession session) throws IOException {
-        String id = getId(request);
-        if (id.isEmpty()) {
-            sendBadRequest(session);
-            return;
-        }
-        try {
-            byte[] body = dao.get(id.getBytes());
-            session.sendResponse(Response.ok(body));
-        } catch (NoSuchElementException ex) {
-            sendNotFound(session);
-        } catch (IOException ex) {
-            sendServerError(session);
-            logger.error("server error", ex);
-        }
+  private void handleGet(Request request, HttpSession session) throws IOException {
+    String id = getId(request);
+    if (id.isEmpty()) {
+      sendBadRequest(session);
+      return;
     }
+    try {
+      byte[] body = dao.get(id.getBytes());
+      session.sendResponse(Response.ok(body));
+    } catch (NoSuchElementException ex) {
+      sendNotFound(session);
+    } catch (IOException ex) {
+      sendServerError(session);
+      logger.error("server error", ex);
+    }
+  }
 
-    private void handlePut(Request request, HttpSession session) throws IOException {
-        String id = getId(request);
-        if (id.isEmpty()) {
-            sendBadRequest(session);
-            return;
-        }
-        try {
-            dao.upsert(id.getBytes(), request.getBody());
-            session.sendResponse(new Response(Response.CREATED, Response.EMPTY));
-        } catch (IOException ex) {
-            sendServerError(session);
-            logger.error("server error", ex);
-        }
+  private void handlePut(Request request, HttpSession session) throws IOException {
+    String id = getId(request);
+    if (id.isEmpty()) {
+      sendBadRequest(session);
+      return;
     }
+    try {
+      dao.upsert(id.getBytes(), request.getBody());
+      session.sendResponse(new Response(Response.CREATED, Response.EMPTY));
+    } catch (IOException ex) {
+      sendServerError(session);
+      logger.error("server error", ex);
+    }
+  }
 
-    private void handleDelete(Request request, HttpSession session) throws IOException {
-        String id = getId(request);
-        if (id.isEmpty()) {
-            sendBadRequest(session);
-            return;
-        }
-        try {
-            dao.remove(id.getBytes());
-            session.sendResponse(new Response(Response.ACCEPTED, Response.EMPTY));
-        } catch (IOException ex) {
-            sendServerError(session);
-            logger.error("server error", ex);
-        }
+  private void handleDelete(Request request, HttpSession session) throws IOException {
+    String id = getId(request);
+    if (id.isEmpty()) {
+      sendBadRequest(session);
+      return;
     }
+    try {
+      dao.remove(id.getBytes());
+      session.sendResponse(new Response(Response.ACCEPTED, Response.EMPTY));
+    } catch (IOException ex) {
+      sendServerError(session);
+      logger.error("server error", ex);
+    }
+  }
 
-    private void handleStatus(HttpSession session) throws IOException {
-        session.sendResponse(Response.ok("Server is running"));
-    }
+  private void handleStatus(HttpSession session) throws IOException {
+    session.sendResponse(Response.ok("Server is running"));
+  }
 
-    private void sendNotFound(HttpSession session) throws IOException {
-        session.sendResponse(new Response(Response.NOT_FOUND, Response.EMPTY));
-    }
+  private void sendNotFound(HttpSession session) throws IOException {
+    session.sendResponse(new Response(Response.NOT_FOUND, Response.EMPTY));
+  }
 
-    private void sendMethodNotAllowed(HttpSession session) throws IOException {
-        session.sendResponse(new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY));
-    }
+  private void sendMethodNotAllowed(HttpSession session) throws IOException {
+    session.sendResponse(new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY));
+  }
 
-    private void sendServerError(HttpSession session) throws IOException {
-        session.sendResponse(new Response(Response.INTERNAL_ERROR, Response.EMPTY));
-    }
+  private void sendServerError(HttpSession session) throws IOException {
+    session.sendResponse(new Response(Response.INTERNAL_ERROR, Response.EMPTY));
+  }
 
-    private void sendBadRequest(HttpSession session) throws IOException {
-        session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
-    }
+  private void sendBadRequest(HttpSession session) throws IOException {
+    session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
+  }
 
-    private String getId(Request request) {
-        String param = request.getParameter("id=");
-        if (param == null){
-            return "";
-        } else {
-            return param;
-        }
+  private String getId(Request request) {
+    String param = request.getParameter("id=");
+    if (param == null) {
+      return "";
+    } else {
+      return param;
     }
+  }
 
-    private String methodToString(Request request) {
-        switch (request.getMethod()) {
-            case Request.METHOD_GET: return "GET";
-            case Request.METHOD_POST: return "POST";
-            case Request.METHOD_HEAD: return "HEAD";
-            case Request.METHOD_OPTIONS: return "OPTIONS";
-            case Request.METHOD_PUT: return "PUT";
-            case Request.METHOD_DELETE: return "DELETE";
-            case Request.METHOD_TRACE: return "TRACE";
-            case Request.METHOD_CONNECT: return "CONNECT";
-            case Request.METHOD_PATCH: return "PATCH";
-            default: return "";
-        }
+  private String methodToString(Request request) {
+    switch (request.getMethod()) {
+      case Request.METHOD_GET:
+        return "GET";
+      case Request.METHOD_POST:
+        return "POST";
+      case Request.METHOD_HEAD:
+        return "HEAD";
+      case Request.METHOD_OPTIONS:
+        return "OPTIONS";
+      case Request.METHOD_PUT:
+        return "PUT";
+      case Request.METHOD_DELETE:
+        return "DELETE";
+      case Request.METHOD_TRACE:
+        return "TRACE";
+      case Request.METHOD_CONNECT:
+        return "CONNECT";
+      case Request.METHOD_PATCH:
+        return "PATCH";
+      default:
+        return "";
     }
+  }
 }
